5a8f629db92ee5c96be8b8bd63922262fcd934da
RishiShethSchoolAccount <10019070@sbstudents.org>
2024-11-28 16:39:29 -0500

    Added Interface
    Added a functional interface that uses the method int calculate(int number1, int number2)

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
-public class main{ 
+public class main{
+	@FunctionalInterface
+	interface Calculate{
+		int calculate(int number1, int number2); 
+	}
 	public static void main(String[] args){ 
-	
+
 	}
 } 
